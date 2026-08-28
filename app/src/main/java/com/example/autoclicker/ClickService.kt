95758adcd5ced1860b5e623ed6b16da7ee194466
Hall-bert <lillonero2000-25@yahoo.it>
2026-08-28 07:53:26 +0200

    Implement ClickService for automated clicks

---
+++ app/src/main/java/com/example/autoclicker/ClickService.kt
@@ -0,0 +1,37 @@
+package com.example.autoclicker
+
+import android.accessibilityservice.AccessibilityService
+import android.accessibilityservice.GestureDescription
+import android.graphics.Path
+import android.view.accessibility.AccessibilityEvent
+
+class ClickService : AccessibilityService() {
+
+    companion object {
+        var instance: ClickService? = null
+    }
+
+    override fun onServiceConnected() {
+        super.onServiceConnected()
+        instance = this
+    }
+
+    override fun onAccessibilityEvent(event: AccessibilityEvent?) {}
+
+    override fun onInterrupt() {
+        instance = null
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        instance = null
+    }
+
+    fun clickAt(x: Float, y: Float) {
+        val path = Path()
+        path.moveTo(x, y)
+        val gestureBuilder = GestureDescription.Builder()
+        gestureBuilder.addStroke(GestureDescription.StrokeDescription(path, 0, 50))
+        dispatchGesture(gestureBuilder.build(), null, null)
+    }
+}
